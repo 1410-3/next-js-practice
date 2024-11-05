9a39082d3def9ec50fd76c2712b5dbc3aa804b35
1410-3 <60973430+1410-3@users.noreply.github.com>
2024-11-05 22:07:05 +0900

    first commit

---
+++ app/components/HelloWorld.tsx
@@ -0,0 +1,16 @@
+
+const HelloWorld = () => {
+  return (
+  <>
+    <p>Hello worldを表示する前のp要素</p>
+      <script>
+        alert( 'Welcome!!' );
+      </script>
+    <p>Hello worldを表示後のp要素</p>
+  </>
+  )
+}
+
+export default HelloWorld
+
+ 
---
+++ app/news/page.tsx
@@ -0,0 +1,12 @@
+
+const News = () => {
+  return (
+  <>
+  oshirase
+  </>
+  )
+}
+
+export default News
+
+ 
